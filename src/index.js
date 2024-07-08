0ebe59c6f8f1d0e8e2244c2c09eda9c21bc0dd93
nassira5832 <nassira.aitmehdi@gmail.com>
2024-07-08 15:13:15 +0200

    bdd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const mongoose=require("mongoose");
 const {handleNotFound, handleInternalServerError, validateRequestBody}= require("./middlewares/FalseKnight")
 const routes= require("./routes/FalseKnight")
 const app=express();
+const  connecttodb = require("./database/main")
+const env= requare("dotenv")
+env.config();
 // challenge01
 app.use(express.json());
 app.use(validateRequestBody);
@@ -14,3 +17,12 @@ const port= 5000;
 app.listen(port, () => {
     console.log(`Serveur API connecté au port ${portAPI}`);
 });
+async function start(){
+    try{
+    await connecttodb(process.env.DatebaseUrl)
+    app.listen(port, ()=>{console.log("my server started ")})
+    }catch(error){
+      console.log("erreur ", error)
+    }};
+   
+ start();
